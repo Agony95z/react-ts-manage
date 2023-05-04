06250bb0e9457a2d90adf724d665607fac80aa77
itw_zhangwh02 <itw_zhangwh02@tkmail.com>
2023-05-04 18:03:44 +0800

    feat:引入antd

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux'
 import {useAppDispatch} from '../../store/index'
 import type {RootState} from '../../store/'
-import { Button, message } from 'antd'
 import { loginAction, updateToken } from '../../store/modules/users'
+import styles from './Login.module.scss'
+import classNames from 'classnames'
+import { Button, Checkbox, Form, Input, message } from 'antd';
 export default function Login() {
   const token = useSelector((state: RootState) => state.users.token)
   const dispatch = useAppDispatch()
@@ -21,10 +23,26 @@ export default function Login() {
       }
     })
   }
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+  
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
-    <div>
-      <Button onClick={handleLogin}>登录</Button>
-      {token}
+    <div className={styles.login}>
+      <div className={styles.header}>
+      <span className={styles['header-logo']}>
+        <i className={classNames('iconfont icon-React', styles['icon-React'])}></i>
+        <i className={classNames('iconfont icon-test', styles['icon-test'])}></i>
+        <i className={classNames('iconfont icon-typescript', styles['icon-typescript'])}></i>
+      </span>
+      <span className={styles['header-title']}>在线考勤系统</span>
+    </div>
+    <div className={styles.desc}>
+      Study React、+ TypeScript
+    </div>
     </div>
   )
 }
