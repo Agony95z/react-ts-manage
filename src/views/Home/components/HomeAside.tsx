1325cc341bd4b1f8e9354082eb57e2bcf465b750
itw_zhangwh02 <itw_zhangwh02@tkmail.com>
2023-05-08 17:11:50 +0800

    feat:侧边菜单开发

--- src/views/Home/components/HomeAside.tsx
+++ src/views/Home/components/HomeAside.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
-
+import {Menu} from 'antd'
+import type { MenuProps } from 'antd';
+import {
+  AppstoreOutlined,
+  ContainerOutlined,
+  DesktopOutlined,
+  MailOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+} from '@ant-design/icons';
+type MenuItem = Required<MenuProps>['items'][number];
+const items: MenuItem[] = [
+  {
+    key: '1',
+    label: '菜单1',
+    icon: <AppstoreOutlined />,
+    children: [
+      {
+        key: '2',
+        label: '菜单1-1',
+        icon: <ContainerOutlined />
+      }
+    ]
+  }
+]
 export default function HomeAside() {
   return (
-    <div>HomeAside</div>
+    <>
+      <Menu
+        defaultSelectedKeys={['2']}
+        defaultOpenKeys={['1']}
+        mode="inline"
+        theme="light"
+        inlineCollapsed={false}
+        items={items}
+      />
+    </>
   )
 }
